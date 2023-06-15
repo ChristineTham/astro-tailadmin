8838cc90accba631dcdec35f363756a35db448ef
Christine Tham <chris.tham@hellotham.com>
2023-06-15 18:02:20 +1000

    Straight port

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
-import sitemap from "@astrojs/sitemap"
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,10 +10,9 @@ export default defineConfig({
     assets: true
   },
   site: 'https://astro-base.netlify.app',
-  integrations: [
-    UnoCSS({
-      injectReset: true
-    }),
-    sitemap()
-  ]
+  integrations: [sitemap(), alpinejs(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: { applyBaseStyles: false },
+  })]
 });
